4d0dba4385858c38e21c1f9c614bba9eb48b61bf
Jonathan Bowden <your-actual-email@example.com>
2026-02-02 11:08:31 -0700

    Initial Commit

---
+++ DailyFlo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyFlo
+//
+//  Created by Jonathan Bowden on 2/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyFlo/DailyFloApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyFloApp.swift
+//  DailyFlo
+//
+//  Created by Jonathan Bowden on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyFloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
